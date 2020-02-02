3a9ef2dd89b20fe7c3533acbc416a78d6c37307c
Yogesh Rawat <yosh.rawat@gmail.com>
2020-02-02 23:02:42 +0530

    git first commit

---
+++ src/main/java/com/yogesh/dsa/DataStructuresAndAlgo/DataStructuresAndAlgoApplication.java
@@ -0,0 +1,13 @@
+package com.yogesh.dsa.DataStructuresAndAlgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructuresAndAlgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructuresAndAlgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yogesh/dsa/DataStructuresAndAlgo/DataStructuresAndAlgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yogesh.dsa.DataStructuresAndAlgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructuresAndAlgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
